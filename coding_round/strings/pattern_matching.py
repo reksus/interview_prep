1e7d0f8665cd83868df9196cf2ffc2d0675f1876
Tarun Tyagi <mail2ttarun@gmail.com>
2024-05-06 00:19:16 +0530

    added pattern matching

--- coding_round/strings/pattern_matching.py
+++ coding_round/strings/pattern_matching.py
@@ -90,7 +90,32 @@ def shortest_palin_rabin_karp(s: str):
         pow = pow*d
     return s[max_palin_prefix_len:][::-1] + s
         
-        
+
+# 1044. Longest Duplicate Substring
+# Given a string s, consider all duplicated substrings: (contiguous) substrings of s that occur 2 or more times. The occurrences may overlap.
+# Return any duplicated substring that has the longest possible length. If s does not have a duplicated substring, the answer is "".
+# def longestDupSubstring(self, S):
+#     A = [ord(c) - ord('a') for c in S]
+#     mod = 2**63 - 1
+
+#     def test(L):
+#         p = pow(26, L, mod)
+#         cur = reduce(lambda x, y: (x * 26 + y) % mod, A[:L], 0)
+#         seen = {cur}
+#         for i in xrange(L, len(S)):
+#             cur = (cur * 26 + A[i] - A[i - L] * p) % mod
+#             if cur in seen: return i - L + 1
+#             seen.add(cur)
+#     res, lo, hi = 0, 0, len(S)
+#     while lo < hi:
+#         mi = (lo + hi + 1) / 2
+#         pos = test(mi)
+#         if pos:
+#             lo = mi
+#             res = pos
+#         else:
+#             hi = mi - 1
+#     return S[res:res + lo]
 
 if __name__ == "__main__":
         
